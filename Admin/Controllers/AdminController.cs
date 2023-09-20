468323321a34fc4df0925a027de43c92b8704ea0
RustaMSHar <rustam.lika@mail.ru>
2023-09-20 23:59:48 +0300

    a little bit changes

--- Admin/Controllers/AdminController.cs
+++ Admin/Controllers/AdminController.cs
@@ -1,6 +1,19 @@
-﻿namespace MetanApi.Admin.AdminController
+﻿using MetanApi.Services;
+using MetanApi.Models;
+using MongoDB.Driver;
+using Microsoft.AspNetCore.Mvc;
+using MetanApi.Admin.Services;
+
+namespace MetanApi.Admin.AdminController
 {
-    public class AdminController
+    [ApiController]
+    [Route("admin")]
+    public class AdminController : ControllerBase
     {
+        private readonly AdminService _adminService;
+        public AdminController(AdminService AdminService) =>
+           _adminService = AdminService;
+
     }
-}
+
+ }
