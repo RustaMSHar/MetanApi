def5bf5570bcfbcb0b1c30a9cdf8231f17043d64
RustaMSHar <rustam.lika@mail.ru>
2023-10-03 09:15:03 +0300

    some little changes

--- Admin/Models/AdminItems.cs
+++ Admin/Models/AdminItems.cs
@@ -8,8 +8,10 @@ namespace MetanApi.Admin.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
+        public string? Address { get; set; }
         public string? Name { get; set; }
         public string? Password { get; set; }
+        public string? Tittle { get; set; }
         public string? Role { get; set; }
 
     }
